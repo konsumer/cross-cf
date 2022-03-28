ae0fd54ac8be9b99bc1e105e79ddfc4377c8d586
David Konsumer <konsumer@jetboystudio.com>
2022-03-27 21:09:37 -0700

    smarter list/get on example

--- example/src/schema.js
+++ example/src/schema.js
@@ -6,9 +6,16 @@ import typeDefs from './typeDefs.gql'
 // KV-based resolvers for pokemon graphql API
 export const resolvers = {
   Query: {
-    async pokemons (_, args, { env: { MYDB } }) {
+    async pokemons (_, args, { env: { MYDB } }, info) {
       const { keys } = await MYDB.list()
-      // TODO: check if they want more than name, if not skip this
+
+      // check if they want more than just names, if not, return faster
+      // https://www.prisma.io/blog/graphql-server-basics-demystifying-the-info-argument-in-graphql-resolvers-6f26249f613a
+      const fields = info.fieldNodes.map(f => f.selectionSet.selections.map(s => s.name.value)).reduce((a, c) => [...a, ...c], [])
+      if (fields.length === 1 && fields[0] === 'name') {
+        return keys.map(({ name }) => ({ name }))
+      }
+
       return Promise.all(keys.map(({ name }) => MYDB.get(name, { type: 'json' })))
     },
     pokemon (_, { name }, { env: { MYDB } }) {
